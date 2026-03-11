434a54305be3f7901c84d43b7528b6341bcdca2b
deiz0n <eduardomacedosilva@outlook.com>
2026-03-11 13:24:02 -0300

    feat: UserModel

---
+++ auth-service/src/main/java/org/deizon/authservice/domain/UserModel.java
@@ -0,0 +1,12 @@
+package org.deizon.authservice.domain;
+
+import java.util.UUID;
+
+public record UserModel(
+        UUID id,
+        String fullName,
+        String cpf,
+        String cnpj,
+        String email,
+        String password
+) {}
