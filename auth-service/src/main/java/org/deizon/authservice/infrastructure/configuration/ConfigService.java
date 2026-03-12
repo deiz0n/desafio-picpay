6619a11b6c945a10d32332b74ed8e4c49cddb703
deiz0n <eduardomacedosilva@outlook.com>
2026-03-12 19:41:08 -0300

    feat: ConfigService

---
+++ auth-service/src/main/java/org/deizon/authservice/infrastructure/configuration/ConfigService.java
@@ -0,0 +1,37 @@
+package org.deizon.authservice.infrastructure.configuration;
+
+import org.deizon.authservice.application.ports.UserRepositoryPort;
+import org.deizon.authservice.application.services.*;
+import org.deizon.authservice.application.useCases.*;
+import org.deizon.authservice.infrastructure.ports.persistence.repositories.UserRepositoryImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigService {
+
+    @Bean
+    public CreateUserUseCase getCreateUserService(UserRepositoryPort repository) {
+        return new CreateUserService(repository);
+    }
+
+    @Bean
+    public DeleteUserUseCase getDeleteUserService(UserRepositoryPort repository) {
+        return new DeleteUserService(repository);
+    }
+
+    @Bean
+    public ReadUsersUseCase getReadUsersService(UserRepositoryPort repository) {
+        return new ReadUsersService(repository);
+    }
+
+    @Bean
+    public ReadUserUseCase getReadUserService(UserRepositoryPort repository) {
+        return new ReadUserService(repository);
+    }
+
+    @Bean
+    public UpdateUserUseCase getUpdateUserService(UserRepositoryPort repository) {
+        return new UpdateUserService(repository);
+    }
+}
