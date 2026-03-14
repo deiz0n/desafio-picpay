72573ee3957c5fbde3bad4e0fe50f3e18109cd18
deiz0n <eduardomacedosilva@outlook.com>
2026-03-14 16:40:11 -0300

    feat: criação de handles personalizados para exceções de AccessDenied e AuthenticationEntryPoint

---
+++ auth-service/src/main/java/org/deizon/authservice/infrastructure/security/exceptions/CustomAccessDeniedHandler.java
@@ -0,0 +1,35 @@
+package org.deizon.authservice.infrastructure.security.exceptions;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import java.io.IOException;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final HandlerExceptionResolver exceptionResolver;
+
+    public CustomAccessDeniedHandler(
+            @Qualifier("handlerExceptionResolver")
+            HandlerExceptionResolver exceptionResolver
+    ) {
+        this.exceptionResolver = exceptionResolver;
+    }
+
+    @Override
+    public void handle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AccessDeniedException accessDeniedException
+    ) throws IOException, ServletException
+    {
+        exceptionResolver.resolveException(request, response, null, accessDeniedException);
+    }
+}
